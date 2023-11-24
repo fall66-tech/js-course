a700d9589a18a4e1eab5737aa5fd895164599372
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-11-24 11:46:18 +0530

    js practice 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// 
+
+
+
+// document.getElementsByTagName("nav")[2].lastElementChild.style.color = "red" ;
+
+
+Array.from( document.getElementsByTagName("li")).forEach((e) => {
+    e.style.background = "red";
+})
