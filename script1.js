54c72ab7543d480ad4e4b1346f02f423a11d204e
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-11-30 11:25:59 +0530

    javascript live clock

--- script1.js
+++ script1.js
@@ -1,3 +1,11 @@
-setInterval(function() {
-    document.querySelector("#bulb").classList.toggle("bulbon");
+
+
+setInterval(function () {
+  let a = new Date();
+  let h = a.getHours();
+  let m = a.getMinutes();
+  let s = a.getSeconds();
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("mins").innerHTML = m;
+  document.getElementById("secs").innerHTML = s;
 }, 1000);
