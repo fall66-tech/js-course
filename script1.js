36cd3f59847ca7c21f36c32f5df826bf51469c21
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-12-17 12:58:24 +0530

    async await

--- script1.js
+++ script1.js
@@ -1,7 +1,31 @@
 async function harry(){
-    return 5
+    let delhiweather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("27 deg")
+        }, 2000);
+    })
+    let bloreweather = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("22 deg")
+        }, 6000);
+    })
+
+    // delhiweather.then(alert)
+    // bloreweather.then(alert)
+    console.log("fetching delhi weather please wait.......... ")
+    let delhiw = await delhiweather
+    console.log("fetched delhi weather: " +delhiw)
+
+
+    console.log("fetching blore weather please wait.......... ")
+    let blorew = await bloreweather
+    console.log("fetched blore weather: " +blorew)
+    return [delhiw, blorew]
 }
 
-harry().then((value)=> {
-    alert(value)
+console.log("welocme to weather station")
+let a = harry()
+
+a.then((value)=>{
+    console.log(value)
 })
