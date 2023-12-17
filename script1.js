ba4ad3d1f065801717a8d090af2d24a5bdcec836
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-12-17 13:16:23 +0530

    throw new error

--- script1.js
+++ script1.js
@@ -1,5 +1,6 @@
 try {
-    rahul
+    // throw new Error("you will be great");
+    throw new ReferenceError("you will be great");
 } catch (error) {
     console.log(error.name)
     console.log(error.message)
