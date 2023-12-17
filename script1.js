e60e9b0c706cca05f03f2761df53176e1432d1cd
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-12-17 13:10:10 +0530

    try catch

--- script1.js
+++ script1.js
@@ -1,31 +1,25 @@
-async function harry(){
-    let delhiweather = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("27 deg")
-        }, 2000);
-    })
-    let bloreweather = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("22 deg")
-        }, 6000);
-    })
+setTimeout(() => {
+    console.log("hacking your phone");
 
-    // delhiweather.then(alert)
-    // bloreweather.then(alert)
-    console.log("fetching delhi weather please wait.......... ")
-    let delhiw = await delhiweather
-    console.log("fetched delhi weather: " +delhiw)
+}, 1000);
 
 
-    console.log("fetching blore weather please wait.......... ")
-    let blorew = await bloreweather
-    console.log("fetched blore weather: " +blorew)
-    return [delhiw, blorew]
+try {
+    console.log(rahul)
+} catch (error) {
+    console.log("error hacking his number")
+    
 }
 
-console.log("welocme to weather station")
-let a = harry()
 
-a.then((value)=>{
-    console.log(value)
-})
+
+setTimeout(() => {
+    console.log("hacking your phone.........");
+   
+}, 3000);
+
+
+setTimeout(() => {
+    console.log("hacked");
+
+}, 5000);
