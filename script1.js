2842b71fcd40171dd8b3b0a57a437eba3b41184e
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-12-18 12:00:18 +0530

    loadscript async await and then

--- script1.js
+++ script1.js
@@ -1,11 +1,35 @@
 const loadscript = async (src) => {
-    let script = document.createElement("script")
-    script.src = src
-    console.log("this is the new script tag")
-    console.log("this is the cdn link " +src)
-    // console.log(script)
-    document.head.append(script)
-    
-}
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.src = src;
+    console.log("this is the new script tag");
+    // console.log("this is the cdn link " + src);
+    script.onload = () => {
+    //   resolve(50);
+      resolve(src + " done success");
+    };
 
-loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js")
+    document.head.append(script);
+  });
+};
+    let a = loadscript(
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+
+);
+a.then((value)=>{
+    console.log(value)
+})
+// const main2 = async ()=> {
+//     console.log(new Date().getMilliseconds())
+//     let a = await loadscript(
+//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+
+// );
+// console.log(new Date().getMilliseconds())
+
+// console.log(a)
+
+// }
+
+// main2()
+
