897634a328f3fad8bea9b362acf4d392db223993
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-12-21 11:38:30 +0530

    fetch api

--- script1.js
+++ script1.js
@@ -1,24 +1,30 @@
-const loadscript = async (src) => {
-  return new Promise((resolve, reject) => {
-    let script = document.createElement("script");
-    script.src = src;
-    console.log("this is the new script tag");
-    // console.log("this is the cdn link " + src);
-    script.onload = () => {
-    //   resolve(50);
-      resolve(src + " done success");
-    };
+// const loadscript = async (src) => {
+//   return new Promise((resolve, reject) => {
+//     let script = document.createElement("script");
+//     script.src = src;
+//     console.log("this is the new script tag");
+//     // console.log("this is the cdn link " + src);
+//     script.onload = () => {
+//     //   resolve(50);
+//       resolve(src + " done success");
+//     };
 
-    document.head.append(script);
-  });
-};
-    let a = loadscript(
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+//     document.head.append(script);
+//   });
+// };
+//     let a = loadscript(
+//   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
 
-);
-a.then((value)=>{
-    console.log(value)
-})
+// );
+// a.then((value)=>{
+//     console.log(value)
+// })
+
+
+
+
+
+
 // const main2 = async ()=> {
 //     console.log(new Date().getMilliseconds())
 //     let a = await loadscript(
@@ -33,3 +39,22 @@ a.then((value)=>{
 
 // main2()
 
+let p =()=>{
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject(new Error("this is not acceptable"))
+    }, 3000);
+})
+
+} 
+let d = async ()=>{
+    try {
+        let c =  await p()
+    console.log(c) 
+    } catch (error) {
+       console.log("this error has been handled")
+    }
+    
+}
+
+d()
